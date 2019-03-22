64f252e8d59dd5bd0277126dd2b1f8d046da6b6c
Petar <petar.pavlovic@quantox.com>
2019-03-22 15:05:07 +0100

    initial

---
+++ precache-manifest.3dcee3dac17e8608f7bdf0a246459dcb.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "3e5aa12886dce3ac3a16",
+    "url": "/static/css/main.169980dd.chunk.css"
+  },
+  {
+    "revision": "3e5aa12886dce3ac3a16",
+    "url": "/static/js/main.3e5aa128.chunk.js"
+  },
+  {
+    "revision": "c834e45226f1e1d5cf2d",
+    "url": "/static/js/1.c834e452.chunk.js"
+  },
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "db49618bcc0c5e2c46cadf3c491cd7bd",
+    "url": "/index.html"
+  }
+];
